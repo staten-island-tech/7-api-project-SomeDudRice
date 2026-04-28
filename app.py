248e628a4539a46d929e953fd4dd133f44da6501
SomeDudRice <Lucasl197@nycstudents.net>
2026-04-28 10:12:42 -0400

    rigamortis

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+def getrick(characters):
+    response = requests.get(f"https://rickandmortyapi.com/api/character/{characters.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    data = response.json()
+    return {
+        "name": data["name"]
+    }
+char =getrick("Rick")
+print(char)
+
+
+# import requests
+
+# def getPoke(poke):
+#     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+#     if response.status_code != 200:
+#         print("Error fetching data!")
+#         return None
+    
+#     data = response.json()
+#     return {
+#         "name": data["name"],
+#         "height": data["height"],
+#         "weight": data["weight"],
+#         "types": [t["type"]["name"] for t in data["types"]]
+#     }
+
+# pokemon = getPoke("Bulbasaur")
+# print(pokemon)
